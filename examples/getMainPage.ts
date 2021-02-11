2d836b368e520b9c46d90be0e0231a8be4171372
cute_panda <asdf3601a@gmail.com>
2021-02-11 15:48:36 +0800

    rewrite a part code.

--- examples/getMainPage.ts
+++ examples/getMainPage.ts
@@ -1,11 +1,12 @@
 import { DianCourse } from "../src/DianCourse"
 
 async function main() {
-    let identify = await DianCourse.auth("Account", "Password")
-    let dianCourse = new DianCourse(identify as string)
-    console.log(identify)
-    let mainPage = await dianCourse.getMainPage() as Buffer
-    console.log(mainPage.toString("utf-8"))
+    let identify = await DianCourse.auth("Account", "Password") as string
+    let dianCourse = new DianCourse(identify)
+    console.log("identify: " + dianCourse.identify)
+    let getMainPage = await dianCourse.getMainPage() as Buffer
+
+    console.log(getMainPage.toString("utf-8"))
 }
 
 main()
--- src/DianCourse.ts
+++ src/DianCourse.ts
@@ -1,17 +1,32 @@
 import * as https from "https"
 import * as cheerio from "cheerio"
-import * as querystring from "querystring"
+
+interface options {
+	hostname: string,
+	port: number,
+	path: string,
+	method: string,
+	headers: object
+}
 
 class DianCourse {
 	identify: string
+	options: options
 
 	constructor(identify: string) {
+		this.options = {
+			hostname: "webs.asia.edu.tw",
+			port: 443,
+			path: "",
+			method: "",
+			headers: {},
+		}
 		this.identify = identify
 	}
 
-	static auth(account: string, password: string) {
+	static auth(id: string, pwd: string) {
 		return new Promise((resolve, reject) => {
-			let postData = "f_id="+account+"&f_pwd="+password
+			let postData = "f_id=" + id + "&f_pwd=" + pwd
 
 			let request = https.request(
 				{
@@ -27,9 +42,8 @@ class DianCourse {
 				(response) => {
 					response.on("data", (chunk) => {
 						let $ = cheerio.load(chunk.toString("utf-8"))
-						let isSuccess = $("a").attr("href") == "main.asp"
 
-						if (response.headers["set-cookie"] && isSuccess) {
+						if (response.headers["set-cookie"] && $("a").attr("href") == "main.asp") {
 							resolve(response.headers["set-cookie"][0].split(";")[0])
 						} else {
 							reject(new Error("Wrong ID or Password."))
@@ -38,84 +52,62 @@ class DianCourse {
 				}
 			)
 
-			request.on("error", (err) => {
-				reject(err)
-			})
-
+			request.on("error", (err) => { reject(err) })
 			request.write(postData)
+			request.end()
+		})
+	}
 
+	getPath(path: string) {
+		return new Promise((resolve, reject) => {
+			let options = this.options
+			options["path"] = path
+			options["method"] = "GET"
+			options["headers"] = { "Cookie": this.identify }
+
+			let request = https.request(
+				options as object,
+				(response) => {
+					let buf: any[] = []
+					response.on("data", (chunk) => { buf.push(chunk) })
+					response.on("error", () => { reject })
+					response.on("close", () => { resolve(Buffer.concat(buf)) })
+				}
+			)
+
+			request.on("error", (err) => { reject(err) })
+			request.end()
+		})
+	}
+
+	postPath(path: string, postData: any) {
+		return new Promise((resolve, reject) => {
+			let options = this.options
+			options["path"] = path
+			options["method"] = "POST"
+			options["headers"] = {
+				"Content-Type": "application/x-www-form-urlencoded",
+				"Content-Length": postData.length,
+				"Cookie": this.identify
+			}
+
+			let request = https.request(
+				options as object,
+				(response) => {
+					let buf: any[] = []
+					response.on("data", (chunk) => { buf.push(chunk) })
+					response.on("error", () => { reject })
+					response.on("close", () => { resolve(Buffer.concat(buf)) })
+				}
+			)
+
+			request.on("error", (err) => { reject(err) })
 			request.end()
 		})
 	}
 
 	getMainPage() {
 		return this.getPath("/stdinfo/main.asp")
-	}
-
-	getPathByID(id: string) {
-		return this.getPath("/stdinfo/main.asp?FID=" + id)
-	}
-
-	getPath(path: string) {
-		return new Promise((resolve, reject) => {
-			let request = https.request(
-				{
-					hostname: "webs.asia.edu.tw",
-					port: 443,
-					path: path,
-					method: "GET",
-					headers: {
-						"Cookie": this.identify
-					}
-				},
-				(response) => {
-					response.on("data", (chunk) => {
-						resolve(chunk)
-					})
-				}
-			)
-
-			request.on("error", (err) => {
-				reject(err)
-			})
-
-			request.end()
-		})
-	}
-
-	postPathByID(id: string, postData: string) {
-		return this.postPath("?FID="+id, postData)
-	}
-
-	postPath(path: string, postData: string) {
-		return new Promise((resolve, reject) => {
-			let request = https.request(
-				{
-					hostname: "webs.asia.edu.tw",
-					port: 443,
-					path: path,
-					method: "POST",
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded",
-						"Content-Length": postData.length,
-						"Cookie": this.identify
-					}
-				},
-				(response) => {
-					response.on("data", (chunk) => {
-						resolve(chunk)
-					})
-				}
-			)
-
-			request.on("error", (err) => {
-				reject(err)
-			})
-	
-			request.write(postData)
-	
-			request.end()	
-		})
 	}
 }
 
